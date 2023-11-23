4884bd34688f8e286e208e3f169ef339800048fb
AutomateMeNow <141515323+AutomateMeNow@users.noreply.github.com>
2023-11-23 18:54:10 +0100

    Create Exctract-all-links.js

---
+++ Exctract-all-links.js
@@ -0,0 +1,5 @@
+https://towardsdatascience.com/quickly-extract-all-links-from-a-web-page-using-javascript-and-the-browser-console-49bb6f48127b
+var w = window.open("");
+w.document.write("<table><thead><th>Name<th>Links<tbody>");
+for(var x of document.querySelectorAll("a"))
+w.document.write( "<tr><td>"+ x.textContent + "<td>"+x.href);
